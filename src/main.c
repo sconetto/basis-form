68541f9ad6fa18498b53761420d4f721dc46d000
João Pedro Sconetto <sconetto.joao@gmail.com>
2016-06-28 09:26:02 -0300

    Minor correction

--- src/main.c
+++ src/main.c
@@ -1,7 +1,6 @@
 #include "form.h"
 
 int main() {
-	int menu;
 	user profile;
 	setlocale(LC_ALL, "Portuguese");
 	system("cls || clear");
@@ -10,12 +9,13 @@ int main() {
 	system("cls || clear");
 	interface();
 	system("cls || clear");
-	menu = 0;
 	profile = read_new_user();
 	sleep(2);
 	
 	system("cls || clear");
 	printf("Informações do novo usuário: \n");
 	show_user(profile);
+	printf("Terminando a execução...\n");
+	printf("Obrigado por usar o sistema!!!\n\n");
 	return 0;
 }
